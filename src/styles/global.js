a32b5200638136ab2b36856751934a712b1c7ba1
Javier <javier.williams720@gmail.com>
2018-09-09 19:02:26 -0700

    updated main styles, using styled-components now

---
+++ src/styles/global.js
@@ -0,0 +1,71 @@
+import { css } from 'styled-components';
+
+
+export const fonts = css`
+	font-family: 'Noto Serif', serif;
+`
+
+export const normalize = css`
+  html,
+  body {
+    font-size: 100%;
+  }
+
+  body {
+    position: relative;
+    -webkit-font-smoothing: antialiased;
+    font-smooth: antialiased;
+    -moz-font-smoothing: antialiased;
+  }
+
+  button {
+    border: 0;
+    border-radius: 0;
+    padding: 0;
+    font-family: inherit;
+    font-size: inherit;
+    color: inherit;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  a,
+  a:link {
+    text-decoration: none;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  ul,
+  ol {
+    margin: 0;
+    padding: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default css`
+	${fonts};
+  ${normalize};
+
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    margin: 0;
+    font-family: 'Noto Serif', serif;
+    font-size: 14px;
+
+`
---
+++ src/styles/theme.js
@@ -0,0 +1,24 @@
+import { css } from 'styled-components';
+
+const colors = {
+	
+}
+
+export default {
+  colors: colors,
+  breakpoints: {
+    small: 0,
+    medium: 414,
+    large: 768,
+    xlarge: 1024,
+    xxlarge: 1440
+  },
+  spacing: {
+    tiny: 8,
+    small: 16,
+    moderate: 24,
+    medium: 32,
+    large: 40,
+    xlarge: 80
+  }
+}
